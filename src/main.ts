c9a691c17863c07260b3ecb3788a84357c0c03bb
oibioib <pavelbodal@gmail.com>
2023-07-26 12:45:34 +0200

    feat: implement port loading from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+const DEFAULT_APP_PORT = 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const configService: ConfigService = app.get<ConfigService>(ConfigService);
+  const port = configService.get('PORT') || DEFAULT_APP_PORT;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
