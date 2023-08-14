ccbabd98937b097aae266e823da0c9a6fecf9c54
oibioib <pavelbodal@gmail.com>
2023-08-14 13:07:54 +0200

    refactor: change port config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,3 @@
 import { ConfigService } from '@nestjs/config';
 
-export const DEFAULT_APP_PORT = 4000;
-
-export const getAppPort = () =>
-  new ConfigService().get('PORT') || DEFAULT_APP_PORT;
+export const getAppPort = () => new ConfigService().get('PORT');
