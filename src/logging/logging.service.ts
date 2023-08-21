fefd3095bac00c40441058e1c2d43f04da303ecb
oibioib <pavelbodal@gmail.com>
2023-08-21 14:10:06 +0200

    refactor: change logging service

--- src/logging/logging.service.ts
+++ src/logging/logging.service.ts
@@ -1,47 +1,40 @@
 import { ConsoleLogger, Injectable } from '@nestjs/common';
-import { colors, colorReset } from './logging.colors';
+import { COLOR_RESET, COLORS } from './logging.colors';
+import { LOG_METHODS, LOG_METHODS_TO_CONSOLE } from './logging.constants';
 
 @Injectable()
 export class LoggingService extends ConsoleLogger {
   log(message: any) {
-    console.log(
-      `${colors.cyan}[ LOG ]${colorReset} [${this._getTime()}] ${message}`,
-    );
+    this._performLogging(LOG_METHODS.LOG, message, COLORS.CYAN);
   }
 
   error(message: any) {
-    console.error(
-      `${colors.red}[ ERROR ]${colorReset} [${this._getTime()}] ${message}`,
-    );
+    this._performLogging(LOG_METHODS.ERROR, message, COLORS.RED);
   }
 
   warn(message: any) {
-    console.warn(
-      `${colors.magenta}[ WARN ]${colorReset} [${this._getTime()}] ${message}`,
-    );
+    this._performLogging(LOG_METHODS.WARN, message, COLORS.MAGENTA);
   }
 
   debug(message: any) {
-    console.debug(
-      `${colors.green}[ DEBUG ]${colorReset} [${this._getTime()}] ${message}`,
-    );
+    this._performLogging(LOG_METHODS.DEBUG, message, COLORS.GREEN);
   }
 
   verbose(message: any) {
-    console.log(
-      `${
-        colors.yellow
-      }[ VERBOSE ]${colorReset} [${this._getTime()}] ${message}`,
-    );
+    this._performLogging(LOG_METHODS.VERBOSE, message, COLORS.YELLOW);
   }
 
   logRequest(message: any) {
-    console.log(
-      `${colors.blue}[ REQUEST ]${colorReset} [${this._getTime()}] ${message}`,
-    );
+    this._performLogging(LOG_METHODS.REQUEST, message, COLORS.BLUE);
   }
 
-  _getTime() {
-    return new Date().toISOString();
+  _performLogging(
+    logMethod: keyof typeof LOG_METHODS,
+    message: string,
+    color: COLORS,
+  ) {
+    const logMessage = `${color}[ ${logMethod} ]${COLOR_RESET} [${new Date().toISOString()}] ${message}`;
+    const consoleMethod = LOG_METHODS_TO_CONSOLE[logMethod];
+    console[consoleMethod](logMessage);
   }
 }
